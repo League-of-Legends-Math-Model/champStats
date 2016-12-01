3177b2b6afed2e9a59f49ad1bb3e68aaea3daa9e
jmrutter <jrwwd@mail.missouri.edu>
2016-12-01 15:22:30 -0600

    The master file.

---
+++ master.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 15:09:50 2016
+
+@author: JK Rutter
+"""
+
+# itempull
+
+dpsArray = [[[0, 0, 0, 0, 0, 0], 0]]
+level = 3
+gold = 1000
+
+detBuilds = determinePossibleBuilds(gold)
+
+cStats = caitStats(level)
+
+for i in range(0, len(detBuilds)):
+    stats = genChampStatMatrix(cStats, detBuilds[i])
+    items = detBuilds[i].getItems()
+    
+    seq = caitAttackArray(stats, items)
+    
+    dps = calcDPS(seq)
+    
+    temp = [detBuilds[i].getItems(), dps]
+    
+    dpsArray.append([temp])
+
