7176fc98497f1b467128bda328298022c5e4245d
jmrutter <jrwwd@mail.missouri.edu>
2016-12-01 21:34:36 -0600

    Updated master, prints the max DPS and all the items in the best build.

--- master.py
+++ master.py
@@ -4,7 +4,8 @@ Created on Thu Dec  1 15:09:50 2016
 
 @author: JK Rutter
 """
-
+import json
+import requests
 # itempull
 
 dpsArray = [[[0, 0, 0, 0, 0, 0], 0]]
@@ -28,3 +29,20 @@ for i in range(0, len(detBuilds)):
     
     dpsArray.append([temp])
 
+organize = [[0, 0]]
+
+for i in range(0, len(dpsArray)):
+    organize.append([dpsArray[i][1], i])
+
+organize.sort(reverse=True)
+
+print("Max DPS: " + str(organize[0][0]))
+
+items = dpsArray[organize[0][1]][0]
+
+for i in range(0, len(items)):
+    itemurl = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/item/" + str(int(items[i])) + "?itemData=all&api_key=836619ee-c877-45d9-b718-ab0eea4ed172"
+    response = requests.get(itemurl)
+    buddy = response.json()
+    name = buddy['name']
+    print(name)
