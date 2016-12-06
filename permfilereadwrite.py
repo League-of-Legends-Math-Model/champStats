dee2d9597b895c3484ed220271f26e6981b88c2a
maxhighsmith <mh5234@Truman.edu>
2016-12-06 14:59:00 -0800

    fix

--- permfilereadwrite.py
+++ permfilereadwrite.py
@@ -1,28 +1,32 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec  1 18:00:45 2016
-
 @author: Max
 """
+
 import pickle;
 import datetime;
+from itempull import *;
+
 goldPerm = 1000;
 fileDate = datetime.datetime.now();
 #newFileBytes = [123, 3, 0, 100];
 #ewFileByteArray = bytearray(newFileBytes);
+
+'''gold = 500
+detBuilds = determinePossibleBuildsFile(0,gold);
 '''
-gold = 1000
-detBuilds = determinePossibleBuildsFile(gold)
-newFile = open("uh.txt", "wb");
+'''
+newFile = open("permFiles/uh.txt", "wb");
 p = pickle.Pickler(newFile);
 p.dump(detBuilds);
 '''
-
-
-fh = open('uh.txt', 'rb');
+fh = open('permFiles/perm40.txt', 'rb');
 up = pickle.Unpickler(fh);
 j = up.load();
-print(j[60][6].profile());
 
-    
+ah = determinePossibleBuilds(400);
+print(ah[0].profile());
+print(j[0].profile());
+
 #print(fileDate);
