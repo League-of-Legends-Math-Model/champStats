d981eee5b633380ab062ec1e5af7f473ff84adfe
jmrutter <jrwwd@mail.missouri.edu>
2016-12-08 15:06:39 -0600

    Sivir attack array.

--- sivirAttackArray.py
+++ sivirAttackArray.py
@@ -58,7 +58,15 @@ rapidfirecannon = np.array([50, 50, 50, 50, 50, 58, 66, 75, 83, 92, 100, 109, 11
 statikkshiv = np.array([50, 50, 50, 50, 50, 56, 61, 67, 72, 77, 83, 88, 94, 99, 104, 110, 115, 120])
 
 def sivirAttackArray(statMatr, itemArray):
-    sequence = np.zeros(shape=(2, totalSteps))
+    #NEW SEQUENCE
+    genDamage = [0, 0, 0]
+    genResist = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    sequence = []
+
+    for i in range(0, totalSteps):
+        sequence.append([0, genDamage, genResist, 0])
+    #NEW SEQUENCE
     
     sivirUrl = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/15?champData=all&api_key=836619ee-c877-45d9-b718-ab0eea4ed172"
     response = requests.get(sivirUrl)
@@ -68,47 +76,58 @@ def sivirAttackArray(statMatr, itemArray):
     attackDelay = 0.625 / (1 + baseStats['attackspeedoffset'])
     baseAD = baseStats['attackdamage'] + baseStats['attackdamageperlevel'] * (statMatr[8] - 1) * (0.685 + 0.0175 * statMatr[8])
     itemEffectBank = [
-    [1018, 0, 0, 0, 0],
-    [1027, 0, 0, 0, 0],
-    [1028, 0, 0, 0, 0],
-    [1036, 0, 0, 0, 0],
-    [1037, 0, 0, 0, 0],
-    [1038, 0, 0, 0, 0],
-    [1042, 0, 0, 0, 0],
-    [1043, 0, 0, 0, 0],
-    [1051, 0, 0, 0, 0],
-    [1053, 0, 0, 0, 0],
-    [2015, 0, 40, 0, 0],
-    [3101, 0, 0, 0, 0],
-    [3133, 0, 0, 0, 0],
-    [3031, .5, 0, 0, 0],
-    [3034, 0, 0, 0, .1],
-    [3035, 0, 0, 0, 0],
-    [3035, 0, 0, 0, .15],
-    [3044, 0, 0, 0, 0],
-    [3046, 0, 0, 0, 0],
-    [3057, 0, baseAD, 0, 0],
-    [3072, 0, 0, 0, 0],
-    [3078, 0, 2*baseAD, 0, 0],
-    [3085, 0, 0, 15, 0],
-    [3086, 0, 0, 0, 0],
-    [3094, 0, rapidfirecannon[statMatr[8]], 0, 0],    
-    [3087, 0, statikkshiv[statMatr[8]], 0, 0],
-    [3508, 0, 0, 0, 0]
+    [1018, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1027, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1028, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1036, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1037, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1038, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1042, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1043, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1051, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1053, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2015, 0, 0, 0, 0, 40, 0, 0, 0],
+    [3101, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3133, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3031, .5, 0, 0, 0, 0, 0, 0, 0],
+    [3034, 0, 0, 0, .1, 0, 0, 0, 0],
+    [3035, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3035, 0, 0, 0, .15, 0, 0, 0, 0],
+    [3044, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3046, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3057, 0, baseAD, 0, 0, 0, 0, 0, 0],
+    [3072, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3078, 0, 2*baseAD, 0, 0, 0, 0, 0, 0],
+    [3085, 0, 0, 15, 0, 0, 0, 0, 0],
+    [3086, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3094, 0, 0, 0, 0, rapidfirecannon[statMatr[8]], 0, 0, 0],    
+    [3087, 0, 0, 0, 0, statikkshiv[statMatr[8]], 0, 0, 0],
+    [3508, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
     # if infinity edge, critDamage = 2.5    
     critDamage = 2    
-    oneShotEnhance = 0
-    regOnHit = 0
+    oneShotPhyEnhance = 0
+    regPhyOnHit = 0
     phyEnhance = 1
+    oneShotMagEnhance = 0
+    regMagOnHit = 0
+    lethality = 0
+    perArmorPen = 0
     
     for i in range(0, len(itemArray)):
         for j in range(0, 27):
             if itemArray[i] == itemEffectBank[j][0]:
                 critDamage = critDamage + itemEffectBank[j][1]
-                oneShotEnhance = oneShotEnhance + itemEffectBank[j][2]
-                regOnHit = regOnHit + itemEffectBank[j][3]
+                oneShotPhyEnhance = oneShotPhyEnhance + itemEffectBank[j][2]
+                regPhyOnHit = regPhyOnHit + itemEffectBank[j][3]
                 phyEnhance = phyEnhance + itemEffectBank[j][4]
+                oneShotMagEnhance = oneShotMagEnhance + itemEffectBank[j][5]
+                regMagOnHit = regMagOnHit + itemEffectBank[j][6]
+                lethality = lethality + itemEffectBank[j][7]
+                if itemEffectBank[j][8] > 0 and perArmorPen > 0:
+                    perArmorPen = perArmorPen * itemEffectBank[j][8]
+                elif itemEffectBank[j][8] > 0:
+                    perArmorPen = itemEffectBank[j][8]
     
     abilityPoints = [0, 0, 0, 0]
     
@@ -125,35 +144,38 @@ def sivirAttackArray(statMatr, itemArray):
         ricochet = (spells[3]['effect'][0][abilityPoints[3] - 1]) / 100
         
     for i in range(0, totalSteps):
-        sequence[0][i] = 1 / statMatr[5]
+        sequence[i][0] = 1 / statMatr[5]
+    
+    #Armor Penetration Modifiers
+    for i in range(0, totalSteps):
+        sequence[i][2] = [0, 0, perArmorPen, lethality, 0, 0, 0, 0]
+    
+    #Attack Types
+    for i in range(0, totalSteps):
+        sequence[i][3] = 0    
     
     critted = rng.random()
     
+    #first ability
     sequence[0][0] = 0
-    sequence[1][0] = boomerangBlade * phyEnhance
+    sequence[0][1] = [boomerangBlade * phyEnhance, 0, 0]
+    sequence[0][3] = 1
     
+    #ricochet attacks
     for i in range(1, 4):        
-        sequence[0][i] = 1 / (statMatr[5] + attackDelay * ricochet)
+        sequence[i][0] = 1 / (statMatr[5] + attackDelay * ricochet)
     
-    sequence[1][1] = (statMatr[4] + regOnHit + oneShotEnhance) * phyEnhance
+    sequence[1][1] = [(statMatr[4] + regPhyOnHit + oneShotPhyEnhance) * phyEnhance, oneShotMagEnhance + regMagOnHit, 0]
     
     if critted <= statMatr[6]:
-        sequence[1][1] = (statMatr[4] * critDamage + regOnHit + oneShotEnhance) * phyEnhance
+        sequence[1][1][0] = (statMatr[4] * critDamage + regPhyOnHit + oneShotPhyEnhance) * phyEnhance
     
     for i in range(2, totalSteps):
         critted = rng.random()
-        sequence[1][i] = (statMatr[4] + regOnHit) * phyEnhance
+        sequence[i][1] = [(statMatr[4] + regPhyOnHit) * phyEnhance, regMagOnHit, 0]
         if critted <= statMatr[6]:
-            sequence[1][i] = (statMatr[4] * critDamage + regOnHit) * phyEnhance
+            sequence[i][1][0] = (statMatr[4] * critDamage + regPhyOnHit) * phyEnhance
     
     # other item changes
     print(sequence)
     return sequence
-
-def sivirCalcDPS(sivirSeq):
-    time = 0
-    damage = 0    
-    for i in range(0, totalSteps):
-        time = time + sivirSeq[0][i]
-        damage = damage + sivirSeq[1][i]
-    return damage / time
