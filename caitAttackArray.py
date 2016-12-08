480c44ef1607cad82d157ee5d96b68749a449231
jmrutter <jrwwd@mail.missouri.edu>
2016-12-08 15:06:10 -0600

    New Caitlyn Attack Array.

--- caitAttackArray.py
+++ caitAttackArray.py
@@ -53,7 +53,7 @@ ITEM BANK
     id == 3085 or #runnans hurricaine
     id == 3086 or # recursive bow
     id == 3094 or #rapid fire cannn
-    id == 3086 or #static shiv
+    id == 3087 or #static shiv
     id == 3508    #ess reav
 """
 totalSteps = 15
@@ -61,7 +61,17 @@ rapidfirecannon = np.array([50, 50, 50, 50, 50, 58, 66, 75, 83, 92, 100, 109, 11
 statikkshiv = np.array([50, 50, 50, 50, 50, 56, 61, 67, 72, 77, 83, 88, 94, 99, 104, 110, 115, 120])
 
 def caitAttackArray(statMatr, itemArray):
-    sequence = np.zeros(shape=(2, totalSteps))
+    #sequence = np.zeros(shape=(2, totalSteps))
+    
+    #NEW SEQUENCE
+    genDamage = [0, 0, 0]
+    genResist = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    sequence = []
+
+    for i in range(0, totalSteps):
+        sequence.append([0, genDamage, genResist, 0])
+    #NEW SEQUENCE
     
     caitUrl = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/51?champData=all&api_key=836619ee-c877-45d9-b718-ab0eea4ed172"
     response = requests.get(caitUrl)
@@ -70,48 +80,80 @@ def caitAttackArray(statMatr, itemArray):
     spells = cait['spells']
     
     baseAD = baseStats['attackdamage'] + baseStats['attackdamageperlevel'] * (statMatr[8] - 1) * (0.685 + 0.0175 * statMatr[8])
+    
+    """
+    Item Effect Bank
+    [0] = item number
+    [1] = crit damage increase
+    [2] = physical damage on hit (cd/one shot)
+    [3] = physical damage on hit (constant)
+    [4] = physical damage increase
+        
+    [5] = magical damage on hit (cd/one shot)
+    [6] = magical damage on hit (constant)
+    
+    [7] = flat armor pen
+    [8] = per armor pen
+    
+    .
+    .
+    .
+    more forthcoming
+    """    
+    
     itemEffectBank = [
-    [1018, 0, 0, 0, 0],
-    [1027, 0, 0, 0, 0],
-    [1028, 0, 0, 0, 0],
-    [1036, 0, 0, 0, 0],
-    [1037, 0, 0, 0, 0],
-    [1038, 0, 0, 0, 0],
-    [1042, 0, 0, 0, 0],
-    [1043, 0, 0, 0, 0],
-    [1051, 0, 0, 0, 0],
-    [1053, 0, 0, 0, 0],
-    [2015, 0, 40, 0, 0],
-    [3101, 0, 0, 0, 0],
-    [3133, 0, 0, 0, 0],
-    [3031, .5, 0, 0, 0],
-    [3034, 0, 0, 0, .1],
-    [3035, 0, 0, 0, 0],
-    [3035, 0, 0, 0, .15],
-    [3044, 0, 0, 0, 0],
-    [3046, 0, 0, 0, 0],
-    [3057, 0, baseAD, 0, 0],
-    [3072, 0, 0, 0, 0],
-    [3078, 0, 2*baseAD, 0, 0],
-    [3085, 0, 0, 15, 0],
-    [3086, 0, 0, 0, 0],
-    [3094, 0, rapidfirecannon[statMatr[8]], 0, 0],    
-    [3087, 0, statikkshiv[statMatr[8]], 0, 0],
-    [3508, 0, 0, 0, 0]
+    [1018, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1027, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1028, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1036, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1037, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1038, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1042, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1043, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1051, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1053, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2015, 0, 0, 0, 0, 40, 0, 0, 0],
+    [3101, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3133, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3031, .5, 0, 0, 0, 0, 0, 0, 0],
+    [3034, 0, 0, 0, .1, 0, 0, 0, 0],
+    [3035, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3035, 0, 0, 0, .15, 0, 0, 0, 0],
+    [3044, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3046, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3057, 0, baseAD, 0, 0, 0, 0, 0, 0],
+    [3072, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3078, 0, 2*baseAD, 0, 0, 0, 0, 0, 0],
+    [3085, 0, 0, 15, 0, 0, 0, 0, 0],
+    [3086, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3094, 0, 0, 0, 0, rapidfirecannon[statMatr[8]], 0, 0, 0],    
+    [3087, 0, 0, 0, 0, statikkshiv[statMatr[8]], 0, 0, 0],
+    [3508, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
     # if infinity edge, critDamage = 2.5    
     critDamage = 2    
-    oneShotEnhance = 0
-    regOnHit = 0
+    oneShotPhyEnhance = 0
+    regPhyOnHit = 0
     phyEnhance = 1
+    oneShotMagEnhance = 0
+    regMagOnHit = 0
+    lethality = 0
+    perArmorPen = 0
     
     for i in range(0, len(itemArray)):
         for j in range(0, 27):
             if itemArray[i] == itemEffectBank[j][0]:
                 critDamage = critDamage + itemEffectBank[j][1]
-                oneShotEnhance = oneShotEnhance + itemEffectBank[j][2]
-                regOnHit = regOnHit + itemEffectBank[j][3]
+                oneShotPhyEnhance = oneShotPhyEnhance + itemEffectBank[j][2]
+                regPhyOnHit = regPhyOnHit + itemEffectBank[j][3]
                 phyEnhance = phyEnhance + itemEffectBank[j][4]
+                oneShotMagEnhance = oneShotMagEnhance + itemEffectBank[j][5]
+                regMagOnHit = regMagOnHit + itemEffectBank[j][6]
+                lethality = lethality + itemEffectBank[j][7]
+                if itemEffectBank[j][8] > 0 and perArmorPen > 0:
+                    perArmorPen = perArmorPen * itemEffectBank[j][8]
+                elif itemEffectBank[j][8] > 0:
+                    perArmorPen = itemEffectBank[j][8]
     
     abilityPoints = [0, 0, 0, 0]
     
@@ -126,50 +168,85 @@ def caitAttackArray(statMatr, itemArray):
     caliberNet = spells[2]['effect'][1][abilityPoints[2]]
     peacemaker = spells[0]['effect'][1][abilityPoints[0]] + (spells[0]['effect'][5][abilityPoints[0]]) * statMatr[4]
     
-    for i in range(0, 15):
-        sequence[0][i] = 1 / statMatr[5]
+    """
+    reductions
+    [0] = flat armor reduction
+    [1] = percent armor reduction
+    [2] = percent armor penetration
+    [3] = lethality
+    [4] = flat mr reduction
+    [5] = percent mr reduction
+    [6] = percent mpenetration
+    [7] = flat mpenetration
+
+    damage
+    [0] = physical
+    [1] = magic
+    [2] = true
+    """
+    #generate basic variables
+    for i in range(0, totalSteps):
+        sequence[i][0] = 1 / statMatr[5]
+        
+    #Armor Penetration Modifiers
+    for i in range(0, totalSteps):
+        sequence[i][2] = [0, 0, perArmorPen, lethality, 0, 0, 0, 0]
+    
+    #Attack Types
+    for i in range(0, totalSteps):
+        sequence[i][3] = 0
     
     critted = rng.random()
     
-    sequence[1][0] = (trapHeadshot + oneShotEnhance + regOnHit)*phyEnhance
+    #first attack
+    sequence[0][1] = [(trapHeadshot + oneShotPhyEnhance + regPhyOnHit)*phyEnhance, oneShotMagEnhance + regMagOnHit, 0]
     
     if critted <= statMatr[6]:
-        sequence[1][0] = (trapCritHS + oneShotEnhance + regOnHit)*phyEnhance
+        sequence[0][1] = [(trapCritHS + oneShotPhyEnhance + regPhyOnHit)*phyEnhance, oneShotMagEnhance + regMagOnHit, 0]
     
-    sequence[0][1] = 0.25
-    sequence[1][1] = caliberNet
+    #second attack
+    sequence[1][0] = 0.25
+    sequence[1][1] = [0, caliberNet, 0]
+    sequence[1][3] = 1
     
-    sequence[1][2] = (headshot + regOnHit)*phyEnhance
+    #third attack
+    sequence[2][1] = [(headshot + regPhyOnHit)*phyEnhance, regMagOnHit, 0]
+    sequence[2][3] = 0
     
     critted = rng.random()
     
     if critted <= statMatr[6]:
-        sequence[1][2] = (critHeadshot + regOnHit)*phyEnhance
+        sequence[2][1][0] = (critHeadshot + regPhyOnHit)*phyEnhance
     
+    #fourth attack
     critted = rng.random()
-    sequence[1][3] = (headshot + regOnHit)*phyEnhance
+    sequence[3][1] = [(headshot + regPhyOnHit)*phyEnhance, regMagOnHit, 0]
     if critted <= statMatr[6]:
-        sequence[1][3] = (critHeadshot + regOnHit)*phyEnhance
+        sequence[3][1][0] = (critHeadshot + regPhyOnHit)*phyEnhance
     
+    #remaining auto attacks
     for i in range(4, totalSteps):
         critted = rng.random()
-        sequence[1][i] = (statMatr[4] + regOnHit)*phyEnhance
+        sequence[i][1] = [(statMatr[4] + regPhyOnHit)*phyEnhance, regMagOnHit, 0]
         if critted <= statMatr[6]:
-            sequence[1][i] = (statMatr[4] * critDamage + regOnHit)*phyEnhance
+            sequence[i][1][0] = (statMatr[4] * critDamage + regPhyOnHit)*phyEnhance
     if totalSteps > 10:
         critted = rng.random()
-        sequence[1][10] = (headshot + regOnHit)*phyEnhance
+        sequence[10][1] = [(headshot + regPhyOnHit)*phyEnhance, regMagOnHit, 0]
         if critted <= statMatr[6]:
-            sequence[1][10] = (critHeadshot + regOnHit)*phyEnhance
+            sequence[10][1][0] = (critHeadshot + regPhyOnHit)*phyEnhance
         
     # other item changes
     print(sequence)
     return sequence
 
-def calcDPS(caitSeq):
+def calcDPS(caitSeq, caitAdjusted):
     time = 0
-    damage = 0    
+    damage = 0
     for i in range(0, totalSteps):
-        time = time + caitSeq[0][i]
-        damage = damage + caitSeq[1][i]
+        time = time + caitSeq[i][0]
+        baseCaitlyn = caitStats(caitAdjusted[8])
+        alldamage = resultAttack(caitSeq[i][1], caitSeq[i][2], caitAdjusted, baseCaitlyn)
+        for i in range(0, 3):
+            damage = damage + alldamage[i]
     return damage / time
